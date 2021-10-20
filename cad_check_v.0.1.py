898068a426a71ff8b04906b115cc6ca995787e75
Alexey Zharinov <alzex3@gmail.com>
2021-10-20 23:06:34 +0300

    Added main function

--- cad_check_v.0.1.py
+++ cad_check_v.0.1.py
@@ -4,7 +4,7 @@ from pprint import pprint
 from deepdiff import DeepDiff
 
 
-def create_cad_base(cad_numbers):
+def get_cad_data(cad_numbers):
     objects_data = {}
     for number in cad_numbers:
 
@@ -13,64 +13,30 @@ def create_cad_base(cad_numbers):
         )
 
         obj_type = resp.json()['type']
-        cn_number = resp.json()['objectCn']
-        address = resp.json()['objectData']['address']['mergedAddress']
-        cost = 0
+        if resp.json()['type'] == 'parcel':
+            obj_type = 'parcelData'
 
-        if resp.json()['type'] == 'construction':
-            cost = resp.json()['objectData']['construction']['cadCostValue']
-
-        elif resp.json()['type'] == 'flat':
-            cost = resp.json()['objectData']['flat']['cadCostValue']
-
-        elif resp.json()['type'] == 'building':
-            cost = resp.json()['objectData']['building']['cadCostValue']
-
-        elif resp.json()['type'] == 'parcel':
-            cost = resp.json()['objectData']['parcelData']['cadCostValue']
-
-        objects_data[cn_number] = {
+        objects_data[resp.json()['objectCn']] = {
             'obj_type': obj_type,
-            'address': address,
-            'cost': cost
+            'address': resp.json()['objectData']['address']['mergedAddress'],
+            'update_date': resp.json()['objectData'][obj_type]['actualDate'],
+            'full_address': resp.json()['objectData']['address']['note'],
+            'created_date': resp.json()['objectData']['dateCreated'],
+            'cad_date': resp.json()['objectData']['dateCreated'],
+            'cost': resp.json()['objectData'][obj_type]['cadCostValue'],
         }
 
+    return objects_data
+
+
+def create_cad_base(numbers):
     with open('cad_base.json', 'w', encoding='utf-8') as f:
-        json.dump(objects_data, f, ensure_ascii=False, indent=4)
+        json.dump(get_cad_data(numbers), f, ensure_ascii=False, indent=4)
     print('Cadastre base successful created!')
 
 
 def check_cad_base(cad_base):
-    objects_data = {}
-    for number in cad_base:
-
-        resp = rq.get(
-            url=f'http://rosreestr.ru/fir_lite_rest/api/gkn/fir_lite_object/{number}'
-        )
-
-        obj_type = resp.json()['type']
-        cn_number = resp.json()['objectCn']
-        address = resp.json()['objectData']['address']['mergedAddress']
-        cost = 0
-
-        if resp.json()['type'] == 'construction':
-            cost = resp.json()['objectData']['construction']['cadCostValue']
-
-        elif resp.json()['type'] == 'flat':
-            cost = resp.json()['objectData']['flat']['cadCostValue']
-
-        elif resp.json()['type'] == 'building':
-            cost = resp.json()['objectData']['building']['cadCostValue']
-
-        elif resp.json()['type'] == 'parcel':
-            cost = resp.json()['objectData']['parcelData']['cadCostValue']
-
-        objects_data[cn_number] = {
-            'obj_type': obj_type,
-            'address': address,
-            'cost': cost
-        }
-
+    objects_data = get_cad_data(cad_base)
     if DeepDiff(objects_data, cad_base):
         print('New data detected!')
         pprint(DeepDiff(objects_data, cad_base))
@@ -81,22 +47,23 @@ def check_cad_base(cad_base):
         print('Saved data is up to date!')
 
 
-try:
-    with open('cad_base.json', encoding='utf-8') as f:
-        print('Cadastre base successful detected!')
-        base = json.load(f)
-        check_cad_base(base)
-
-
-except IOError:
-    print('Cadastre base does not detected! Trying to find cadastre numbers list.')
+def main():
     try:
-        with open('cad_numbers.txt', encoding='utf-8') as f:
-            print('Cadastre numbers list detected!')
-            numbers = []
-            for num in f:
-                numbers.append(num.strip())
-            create_cad_base(numbers)
-
+        with open('cad_base.json', encoding='utf-8') as f:
+            print('Cadastre base successful detected!')
+            base = json.load(f)
+            check_cad_base(base)
     except IOError:
-        print('Cadastre numbers list does not detected!')
+        print('Cadastre base does not detected! Trying to find cadastre numbers list.')
+        try:
+            with open('cad_numbers.txt', encoding='utf-8') as f:
+                print('Cadastre numbers list detected!')
+                cad_nums = []
+                for num in f:
+                    cad_nums.append(num.strip())
+                create_cad_base(cad_nums)
+        except IOError:
+            print('Cadastre numbers list does not detected!')
+
+
+main()
